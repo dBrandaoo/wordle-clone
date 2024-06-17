238179b4ad873e196d56a8dbcafc9148130ee340
Diogo Brandão <dbrandao1139@gmail.com>
2024-06-17 23:38:30 +0100

    Added "guess-area" generation

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const kbSecondRow = ["A", "S", "D", "F", "G", "H", "J", "K", "L"]
 // missing backspace key
 const kbThirdRow = ["ENTER", "Z", "X", "C", "V", "B", "N", "M"]
 
+// generates the keyboard
 keyboardRows.forEach(row => {
     if (row.classList.contains("row-1")) {
         for (let i = 0; i < kbFirstRow.length; i++) {
@@ -41,3 +42,22 @@ keyboardRows.forEach(row => {
         row.appendChild(key)
     }
 })
+
+
+const guesses = document.querySelectorAll(".guess")
+
+guesses.forEach(guess => {
+    for (let i = 0; i < 5; i++) {
+        
+    }
+})
+
+// generate guess rows
+for (let i = 0; i < guesses.length; i++) {
+    for (let j = 0; j < 5; j++) {
+        const letter = document.createElement("div")
+        letter.setAttribute("id", `guess${i}-letter${j}`)
+        letter.classList.add("letter")
+        guesses[i].appendChild(letter)
+    }
+}
